805a7712e32d433571854cce922ad49b7593158d
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-27 10:47:02 +0800

    create

---
+++ basic.cpp
@@ -0,0 +1,52 @@
+/*
+[Q]
+[]
+*/
+
+/*include*/
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<map>
+#include<set>
+
+/*using namespace*/
+using namespace std;
+
+/*define type*/
+#define ulli unsigned long long int
+#define lli long long int
+#define pii pair<int,int>
+#define plli pair<lli,lli>
+#define pulli pair<ulli,ulli>
+#define INT int
+#define UINT unsigned INT
+
+/*struct*/
+
+/*num*/
+
+/*fn*/
+
+/*main*/
+int main(){
+	cin.tie(0);
+	cout.tie(0);
+	ios::sync_with_stdio(false);
+	return 0;
+}
+
+/*
+[I1]
+[O1]
+*/
+
+/*think*/
+/*
+
+*/
