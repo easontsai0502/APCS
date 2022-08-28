4514bc7b4c42efa8128ba27263d4d33bc2fbc7d4
ET01 <EasonTsai0502@gmail.com>
2022-08-28 14:40:02 +0800

    a

---
+++ 2022-06-4.cpp
@@ -0,0 +1,74 @@
+/*
+[Q]
+[]
+*/
+
+/*include*/
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<string>
+#include<sstream>
+#include<vector>
+#include<queue>
+#include<deque>
+#include<map>
+#include<set>
+
+/*using namespace*/
+using namespace std;
+
+/*define type*/
+#define ulli unsigned long long int
+#define lli long long int
+#define INT int
+#define UINT unsigned INT
+#define pii pair<INT,INT>
+
+/*struct*/
+
+/*num*/
+const UINT maxnm=1000
+UINT n,m;
+INT a[maxnm+1],b[maxnm+1],br[maxnm+1],ans;
+/*fn*/
+void solver(){
+
+}
+
+void solverbr(){
+	
+}
+
+/*main*/
+int main(){
+	{/*IO 加速*/
+		cin.tie(0);
+		cout.tie(0);
+		ios::sync_with_stdio(false);
+	}
+	{/*CIN*/
+		cin>>n>>m;
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+		}
+		for(int i=0;i<m;i++){
+			cin>>b[i];
+			br=b[m-i-1];
+		}
+	}
+	{/*solve*/
+
+	}
+	return 0;
+}
+
+/*
+[I1]
+[O1]
+*/
+
+/*think*/
+/*
+
+*/
