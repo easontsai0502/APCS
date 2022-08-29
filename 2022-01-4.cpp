82423c1ff5450977603e7ec2c5969fc69bed47a0
ET01 <EasonTsai0502@gmail.com>
2022-08-29 20:02:30 +0800

    a

--- 2022-01-4.cpp
+++ 2022-01-4.cpp
@@ -43,15 +43,31 @@ UINT listfinder(UINT i){
 	}
 	return p;
 }
-bool solver(INT p){
-	UINT now=0,currcount=0;;
-	bool currlist[k];
+bool solver(UINT p){
+	UINT now=0,currcount=0;
+	map<UINT,bool> currlist;
 	for(int i=0;i<n;i++){
 		if(h[i]>=p){
 			now++;
 		}else{
-
+			UINT f=listfinder(now);
+			while(!(currlist[f]) && f){
+				f--;
+			}
+			if(f>=0){
+				currlist[f]=true;
+				currcount++;
+			}
+			now=0;
 		}
+		if(!(currcount-k)){
+		    break;
+		}
+	}
+	if(currcount-k){
+		return false;
+	}else{
+		return true;
 	}
 }
 /*main*/
@@ -76,16 +92,36 @@ int main(){
 		for(UINT i=0;i<k;i++){
 			cin>>w[i];
 		}
+		sort(&(w[0]),&(w[k]));
 	}
+	maxheigh++;
 	{/*solve*/
-
+		while(maxheigh-minheigh-1){
+		    cout<<maxheigh<<","<<minheigh<<"\n";
+			if(solver((maxheigh+minheigh+1)/2)){
+				minheigh=(maxheigh+minheigh+1)/2;
+			}else{
+				maxheigh=(maxheigh+minheigh+1)/2;
+			}
+		}
 	}
+	cout<<minheigh;
 	return 0;
 }
 
 /*
 [I1]
+5 1
+6 3 7 5 1
+3
 [O1]
+3
+[I2]
+10 3
+5 3 7 5 1 7 5 3 8 4
+2 2 1
+[O2]
+5
 */
 
 /*think*/
