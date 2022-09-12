701cfe3b5335947405040ca603eb8bf342df8ca6
ET01 <EasonTsai0502@gmail.com>
2022-09-12 21:13:41 +0800

    a

--- 2018-10-3.cpp
+++ 2018-10-3.cpp
@@ -34,12 +34,28 @@ using namespace std;
 /*fn宣告*/
 
 /*num*/
-string a[200005];
-unordered_map<string,INT> ma;
-INT m,n;
-INT ans;
+string s;
+INT n;
+INT p;
 /*fn定義*/
-
+INT solve(INT x){
+	INT re=0;
+	if(s[p]=='2'){
+		p++;
+		int runtime=4;
+		while(runtime--){
+			re+=solve(x/2);
+		}
+		return re;
+	}else{
+		if(s[p]=='1'){
+			p++;
+			return x*x;
+		}
+	}
+	p++;
+	return 0;
+}
 /*main*/
 int main(){
 	{/*IO加速*/
@@ -48,28 +64,12 @@ int main(){
 		ios::sync_with_stdio(false);
 	}
 	{/*CIN*/
-		cin>>m>>n;
-		for(INT i=0;i<n;i++){
-			cin>>a[i];
-		}
+		cin>>s;
+		cin>>n;
 	}
 	{/*solve*/
-		INT blockcolorcount=0;
-		for(INT i=0;i<m;i++){
-			if(!ma[a[i]])blockcolorcount++;
-			ma[a[i]]++;
-		}
-		ans+=(blockcolorcount==m);
-		for(INT L=0,R=m;R<n;L++,R++){
-			//偵查範圍為L+1到R(含)
-			ma[a[L]]--;
-			if(ma[a[L]]==0)blockcolorcount--;//如果把L砍掉後，該顏色在區間只剩0，則總顏色數--
-			if(ma[a[R]]==0)blockcolorcount++;//如果R那格沒有重複顏色，就++
-			ma[a[R]]++;
-			ans+=(blockcolorcount==m);
-		}
+		cout<<solve(n);
 	}
-	cout<<ans;
 	return 0;
 }
 
@@ -80,6 +80,5 @@ int main(){
 
 /*think*/
 /*
-直接用string的原因是因為它有可能會超過ULLI的範圍
-(max=10^150)
+
 */
